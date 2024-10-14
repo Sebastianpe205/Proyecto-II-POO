4e4fd99275b3d6953ca65ec418719085458b9f0e
sebas <sebas@EquipoSebas205>
2024-10-14 14:14:28 -0600

    Primera carga

--- src/main/java/com/mycompany/proyecto/ii/poo/ProyectoIIPOO.java
+++ src/main/java/com/mycompany/proyecto/ii/poo/ProyectoIIPOO.java
@@ -9,6 +9,8 @@ package com.mycompany.proyecto.ii.poo;
  * @author sebas
  */
 public class ProyectoIIPOO {
+    // Proyecto II de Programacion Orientada a Objetos.
+    // Sebastian Padilla Escalante y Alejandro Monge Arguedas.
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
