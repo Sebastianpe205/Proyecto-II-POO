ce928e00863c60b704851a70c3fc91e97d2cb913
sebas <sebas@EquipoSebas205>
2024-10-19 21:43:16 -0600

    Primera carga

--- src/main/java/com/mycompany/proyecto/ii/poo/ProyectoIIPOO.java
+++ src/main/java/com/mycompany/proyecto/ii/poo/ProyectoIIPOO.java
@@ -17,6 +17,9 @@ public class ProyectoIIPOO {
     public static void main(String[] args) {
         System.out.println("Programa iniciado"); // Se imprime en consola la indicacion de que arranca el programa.
         
+        ReproductorMusica musicaPrincipal = new ReproductorMusica();
+        musicaPrincipal.reproducirMusica("src/main/resources/Musica/musicaTituloMP7.WAV", 1);
+        
         // Se crea la pantalla del menu principal.
         MenuPrincipal menu = new MenuPrincipal();
         //menu.setBackground(Color.white);
---
+++ src/main/java/com/mycompany/proyecto/ii/poo/ReproductorMusica.java
@@ -0,0 +1,55 @@
+package com.mycompany.proyecto.ii.poo;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+import javax.sound.sampled.*;
+import java.io.File;
+import java.io.IOException;
+
+/**
+ *
+ * @author sebas
+ */
+public class ReproductorMusica {
+    private Clip tema;
+    
+    public void reproducirMusica(String ubicacion, int codigo){
+        try {
+            File archivoMusica = new File(ubicacion);
+            if (archivoMusica.exists()){
+                System.out.println("Existe archivo de musica");
+                AudioInputStream streamAudio = AudioSystem.getAudioInputStream(archivoMusica);
+                tema = AudioSystem.getClip();
+                tema.open(streamAudio);
+                
+                // Si el codigo es 0, se reproduce en bucle. Si es 1, solo se reproduce una vez.
+                switch (codigo){
+                    case 0:
+                        tema.loop(Clip.LOOP_CONTINUOUSLY);
+                        tema.start();
+                        break;
+                    case 1:
+                        tema.start();
+                        break;
+                    default:
+                        tema.start(); // Si no es ninguno de los codigos anteriores, simplemente se reproduce una vez.
+                        // Se implementa asi para poder agregar mas codigos, de ser necesario.
+                }
+            } else {
+                ;
+                System.out.println("No existe archivo de musica");
+            }
+        } catch (Exception e) {
+            ; // No hace nada, solo no se reproduce la musica. El ';' vacio es equivalente al 'pass' en Python.
+            System.out.println("Error");
+        }
+    }
+    
+    public void detenerMusica(){
+        if (tema != null && tema.isRunning())
+            tema.stop();
+    }
+    
+}
