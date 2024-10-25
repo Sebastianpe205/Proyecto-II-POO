ddc4c9ab7fe1c8dc1c6ce84402dfa85ef58f3de8
sebas <sebas@EquipoSebas205>
2024-10-25 17:14:16 -0600

    Primera carga

--- src/main/java/com/mycompany/proyecto/ii/poo/PantallasManager.java
+++ src/main/java/com/mycompany/proyecto/ii/poo/PantallasManager.java
@@ -10,10 +10,26 @@ package com.mycompany.proyecto.ii.poo;
  */
 public class PantallasManager {
     // Maneja la apertura y el cierre de las pantallas del juego.
+    // Tambien les agrega musica de fondo.
+    MenuPrincipal p_principal = new MenuPrincipal();
+    ReproductorMusica musicaPrincipal = new ReproductorMusica();
     
     // Constructor.
     public PantallasManager() {
-        MenuPrincipal p_principal = new MenuPrincipal();
+        
+    }
+    
+    // Metodos de pantallas.
+    
+    void mostrarMenuPrincipal(){
+        // Muestra el menu principal.
+        this.p_principal.setVisible(true);
+        this.musicaPrincipal.reproducirMusica("src/main/resources/Musica/musicaTituloMP7.WAV", 1);
+    }
+    
+    void cerrarMenuPrincipal(){
+        // Cierra el menu principal.
+        this.p_principal.dispose();
     }
     
 }
--- src/main/java/com/mycompany/proyecto/ii/poo/ProyectoIIPOO.java
+++ src/main/java/com/mycompany/proyecto/ii/poo/ProyectoIIPOO.java
@@ -17,13 +17,17 @@ public class ProyectoIIPOO {
     public static void main(String[] args) {
         System.out.println("Programa iniciado"); // Se imprime en consola la indicacion de que arranca el programa.
         
-        ReproductorMusica musicaPrincipal = new ReproductorMusica();
-        musicaPrincipal.reproducirMusica("src/main/resources/Musica/musicaTituloMP7.WAV", 1);
+//        ReproductorMusica musicaPrincipal = new ReproductorMusica();
+//        musicaPrincipal.reproducirMusica("src/main/resources/Musica/musicaTituloMP7.WAV", 1);
         
-        // Se crea la pantalla del menu principal.
-        MenuPrincipal menu = new MenuPrincipal();
-        //menu.setBackground(Color.white);
-        menu.setVisible(true); // Su resolucion/dimension es de 1280x720p
+        // Manager de pantallas.
+        PantallasManager p_manager = new PantallasManager();
+        p_manager.mostrarMenuPrincipal();
+        
+//        // Se crea la pantalla del menu principal.
+//        MenuPrincipal menu = new MenuPrincipal();
+//        //menu.setBackground(Color.white);
+//        menu.setVisible(true); // Su resolucion/dimension es de 1280x720p
         int x = (int) (Math.random() * 101); // Ejemplo de como elegir un numero al azar entre 0 y 100.
         System.out.println("Prueba de numero random (0-100): " + x);
         
