cf2bc919b1d627823f0c62e33f4a04b98bc74c5b
sebas <sebas@EquipoSebas205>
2024-10-25 16:59:40 -0600

    Primera carga

---
+++ src/main/java/com/mycompany/proyecto/ii/poo/PantallasManager.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.proyecto.ii.poo;
+
+/**
+ *
+ * @author sebas
+ */
+public class PantallasManager {
+    // Maneja la apertura y el cierre de las pantallas del juego.
+    
+    // Constructor.
+    public PantallasManager() {
+    }
+    
+}
