1aa99b46be0a591287ac7133d523c91af38c5f39
sebas <sebas@EquipoSebas205>
2024-11-08 15:48:11 -0600

    Primera carga

--- src/main/java/ServidorMP/PantallaServidor.java
+++ src/main/java/ServidorMP/PantallaServidor.java
@@ -121,11 +121,11 @@ public class PantallaServidor extends javax.swing.JFrame {
         //</editor-fold>
 
         /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new PantallaServidor().setVisible(true);
-            }
-        });
+//        java.awt.EventQueue.invokeLater(new Runnable() {
+//            public void run() {
+//                new PantallaServidor().setVisible(true);
+//            }
+//        });
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
